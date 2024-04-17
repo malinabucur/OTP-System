0ebf45ce92ea54537ffc0c8b260c3e4d885f4fcf
malinabucur <malinabucur13@gmail.com>
2024-04-17 16:42:03 +0300

    Add project files.

---
+++ OTP System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OTP_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
