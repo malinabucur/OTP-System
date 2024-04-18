55e6e396d6f2ac2182e77ecbc86d980925845b4b
malinabucur <malinabucur13@gmail.com>
2024-04-18 18:35:51 +0300

    implement controller + create reponse model

--- OTP System/Controllers/HomeController.cs
+++ OTP System/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
-﻿using EntityFramework;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
 using OTP_System.Models;
+using BusinessLogic;
 using System.Diagnostics;
 
 namespace OTP_System.Controllers
@@ -9,18 +8,74 @@ namespace OTP_System.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly OtpsystemContext _context;
+        private readonly Service _service;
 
-        public HomeController(ILogger<HomeController> logger, OtpsystemContext otpsystemContext)
+        public HomeController(ILogger<HomeController> logger, Service service)
         {
             _logger = logger;
-            _context = otpsystemContext;
+            _service = service;
         }
 
         public IActionResult Index()
         {
-            var employee = _context.Users.ToList();
             return View();
+        }
+
+        public IActionResult LoggedIn()
+        {
+            return View("LoggedIn");
+        }
+
+        [HttpPost]
+        public IActionResult GenerateTOTP(long id)
+        {
+            try
+            {
+                var totp = _service.GetOtpCode(id);
+
+                return Json(new ResponseModel()
+                {
+                    Value = totp,
+                    Message = "Succesfully retrieved otp code"
+                });
+            }
+            catch(Exception e)
+            {
+                throw new HttpRequestException("OPT Generate failed" ,e ,System.Net.HttpStatusCode.InternalServerError);
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult ValidateTOTP(string totp, int userId)
+        {
+            try
+            {
+
+                var validated = _service.ValidateOTP(totp, userId);
+                if (validated)
+                {
+                    return Json(new ResponseModel()
+                    {
+                        Success = true,
+                        Value = totp,
+                        Message = "Succesfully validated OTP code"
+                    });
+                }
+                else
+                {
+                    return Json(new ResponseModel()
+                    {
+                        Success = false,
+                        //Value = totp,
+                        Message = "Failed to validate OTP code"
+                    });
+                }
+            }
+            catch(Exception e)
+            {
+                throw new HttpRequestException("OPT Generate failed" ,e , System.Net.HttpStatusCode.InternalServerError);
+            }
         }
 
         public IActionResult Privacy()
---
+++ OTP System/Models/ResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OTP_System.Models
+{
+    public class ResponseModel
+    {
+        public string Value { get; set; }
+        public string Message { get; set; }
+        public bool Success { get; set; }
+    }
+}
